23e90af1ceb8f3a15e4d1d2162ea1cd8c3ad50e6
Joao Victor Gomes da Silva <joao.victor@fomento.agn>
2026-09-21 09:39:39 -0300

    Refinei o código do Menu-Streamlit.py, agora ele está tirando os faciais e mudando para a página de Hash

--- Complementos.py
+++ Complementos.py
@@ -1,10 +1,15 @@
 import os
 import datetime
+import time
 def limpar_tela():
     os.system('cls')
 
 def data():
     # Retorna a data atual no formato dd/mm/aaaa
-    return datetime.datetime.now().strftime("%d/%m/%Y")
+    return datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
+
+def wait(tempo):
+    # Pausa a execução do programa por 2 segundos
+    time.sleep(tempo)
 
 print(data())
--- Menu-Streamlit.py
+++ Menu-Streamlit.py
@@ -19,3 +19,20 @@ if st.session_state.tela == "inicio":
 
     with midcol2:
         face = st.camera_input("Capture sua foto para gerar o hash facial", key="camera_input")
+
+        if face is not None:
+            face_name = comp.data() + ".jpg"
+            
+            with open(f"./Faciais/{face_name}", "wb") as arquivo:
+                arquivo.write(face.getbuffer())
+
+            st.success("Foto salva!")
+
+            comp.wait(2)
+
+            st.session_state.tela = "gerar_hash"
+            st.rerun()
+
+if st.session_state.tela == "gerar_hash":
+
+    st.write("Tela de Gerar Hash")
